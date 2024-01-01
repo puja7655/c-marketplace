800cd19f14083fb6ce0493dc0038b085d59af64b
Puja Singh <puja.singh02@sap.com>
2024-01-01 23:05:30 +0530

    Added missing configuration files.

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = {
+    images: {
+      domains: ['ceezer-public-assets.s3.eu-central-1.amazonaws.com'],
+    },
+  }
